9167702dc132010e2e75c53c81a3d246d1a1ab08
tsumk1 <n76474534@gmail.com>
2025-10-14 17:06:20 +0000

    Додала четверту задачу

---
+++ numberFour.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+int main () {
+
+        int a;
+        std::cout<< "Enter number: "<<std::endl;
+        std::cin>> a ; 
+
+
+        std::string str = std::to_string(a); // Переводить  a в рядок 
+        std::cout << str << std::endl;  
+
+        return 0;
+        
+}       
+                                                                                                                                                                                       
+                                                                                                                                                                                       
+                                                                                                                                                                                       
+                                                
